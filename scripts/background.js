84e0512f0ec6a6ba0a5ebbb3c59cb96044245d04
Nikola Popov <nikola.popov01@sap.com>
2020-05-12 01:42:19 +0300

    Initial commit

---
+++ scripts/background.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const javaSuffix = ".java";
+const mavenFileStructurePattern = "src/(main|test)/java";
+const sourcePattern = "src/";
+const sapCorePreffixPattern = "com/sap/core";
+const forwardSlashPattern = /\//g;
+
+function isMavenStructured(path) {
+    return path.search(mavenFileStructurePattern) !== -1;
+}
+
+function toIDEPathIfNeccessary(path) {
+    if (!path.endsWith(javaSuffix)) {
+        return path;
+    }
+
+    let idePath = path;
+    idePath = idePath.trim();
+    if (isMavenStructured(idePath)) {
+        idePath = idePath.slice(idePath.search(mavenFileStructurePattern));
+    } else {
+        idePath = idePath.slice(idePath.lastIndexOf(sourcePattern));
+    }
+    idePath = idePath.slice(idePath.indexOf(sapCorePreffixPattern));
+    idePath = idePath.replace(forwardSlashPattern, ".");
+
+    idePath = idePath.substring(0, idePath.lastIndexOf(javaSuffix));
+
+    return idePath;
+}
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        const lineNumber = request.lineNumber;
+        const file = toIDEPathIfNeccessary(request.file);
+
+        sendResponse({ message: `${file}:${lineNumber}` });
+    }
+);
---
+++ scripts/clipper.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const tickImage = new Image(12, 12);
+tickImage.src = chrome.extension.getURL("/images/tick.png");
+tickImage.title = "Copied";
+
+const tickLine = (cells, sideClassSelector) => {
+  for (let i = 0; i < cells.length; i++) {
+    let currentCell = cells.item(i);
+    if (currentCell.classList.contains("lineNum") && currentCell.classList.contains(sideClassSelector)) {
+      currentCell.appendChild(tickImage);
+    }
+  }
+};
+
+const onMessageReceiveCopyClipboard = async (response) => {
+  try {
+    await navigator.clipboard.writeText(response.message);
+  } catch (err) {
+    console.error('Failed to copy: ', err);
+  }
+};
+
+const fetchFilePath = () => {
+  return document.querySelector("#triggerText").innerText;
+};
+
+const lineSelectCallback = async (mutationsList, observer) => {
+  for (let mutation of mutationsList) {
+    if (mutation.type === "attributes" && mutation.attributeName === "class") {
+      const selectedTableRow = mutation.target;
+      const cells = selectedTableRow.cells;
+      selectedTableRow.classList.contains("target-side-right") ? tickLine(cells, "right") : tickLine(cells, "left");
+
+      const lineNumber = selectedTableRow.cells.item(0).attributes["data-line-number"].value;
+      const file = fetchFilePath();
+      chrome.runtime.sendMessage({ lineNumber, file }, onMessageReceiveCopyClipboard);
+    }
+  }
+};
+
+const observer = new MutationObserver(lineSelectCallback);
+const registerLineObserver = async () => {
+  const diffTableChild = await elementReady("#diffTable > tbody:nth-child(2)");
+  const diffTableRows = diffTableChild.parentNode.tBodies;
+  for (let row of diffTableRows) {
+    observer.observe(row, { attributes: true, subtree: true });
+  }
+};
+registerLineObserver();
+
+new MutationObserver(() => {
+  observer.disconnect();
+  registerLineObserver();
+}).observe(document.body, { subtree: true, childList: true });
+
+/**
+ * MIT Licensed
+ * Author: jwilson8767
+ * 
+ * Waits for an element satisfying selector to exist, then resolves promise with the element.
+ * Useful for resolving race conditions.
+ *
+ * @param selector
+ * @returns {Promise}
+ */
+function elementReady(selector) {
+  return new Promise((resolve, reject) => {
+    let el = document.querySelector(selector);
+    if (el) { resolve(el); }
+    new MutationObserver((mutationRecords, observer) => {
+      Array.from(document.querySelectorAll(selector)).forEach((element) => {
+        resolve(element);
+        observer.disconnect();
+      });
+    })
+      .observe(document.documentElement, {
+        childList: true,
+        subtree: true
+      });
+  });
+}
+
